49647934ecc4f661af310cf5d35f7e2e0fcd488c
PowershellJunkie <86009671+PowershellJunkie@users.noreply.github.com>
2023-09-20 15:24:27 -0400

    Create ClearLocalBrowserCaches.ps1

---
+++ ClearLocalBrowserCaches.ps1
@@ -0,0 +1,115 @@
+Write-Host -ForegroundColor yellow "#######################################################"
+""
+Write-Host -ForegroundColor Green "Visual Powershell tool to delete cache & cookies in Firefox, Chrome, IE and Edge (Chromium) browsers"
+Write-Host -ForegroundColor Green "Original by Lee Bhogal, Remix (v.3.0+) by PowershellJunkie"
+Write-Host -ForegroundColor Green "VERSION: 3.0"
+""
+Write-Host -ForegroundColor yellow "#######################################################"
+""
+Write-Host -ForegroundColor Green "CHANGE_LOG:
+v3.0: - Added support for Edge Chromium, added line to remove users CSV when script completes, added lines to remove 'Snapshots'
+v2.4: - Resolved *.default issue, issue was with the file path name not with *.default, but issue resolved
+v2.3: - Added Cache2 to Mozilla directories but found that *.default is not working
+v2.2: - Added Cyan colour to verbose output
+v2.1: - Added the location 'C:\Windows\Temp\*' and 'C:\`$recycle.bin\'
+v2:   - Changed the retrieval of user list to dir the c:\users folder and export to csv
+v1:   - Compiled script"
+""
+Write-Host -ForegroundColor yellow "#######################################################"
+""
+#########################
+"-------------------"
+Write-Host -ForegroundColor Green "SECTION 1: Getting the list of users"
+"-------------------"
+# Write Information to the screen
+Write-Host -ForegroundColor yellow "Exporting the list of users to c:\users\%username%\users.csv"
+
+# List the users in c:\users and export to the local profile for calling later
+dir C:\Users | select Name | Export-Csv -Path C:\users\$env:USERNAME\users.csv -NoTypeInformation
+$list=Test-Path C:\users\$env:USERNAME\users.csv
+""
+#########################
+"-------------------"
+Write-Host -ForegroundColor Green "SECTION 2: Beginning Script..."
+"-------------------"
+if ($list) {
+    "-------------------"
+    #Clear Mozilla Firefox Cache
+    Write-Host -ForegroundColor Green "SECTION 3: Clearing Mozilla Firefox Caches"
+    "-------------------"
+    Write-Host -ForegroundColor yellow "Clearing Mozilla caches"
+    Write-Host -ForegroundColor cyan
+    Import-CSV -Path C:\users\$env:USERNAME\users.csv -Header Name | foreach {
+            Remove-Item -path C:\Users\$($_.Name)\AppData\Local\Mozilla\Firefox\Profiles\*.default\cache\* -Recurse -Force -EA SilentlyContinue -Verbose
+            Remove-Item -path C:\Users\$($_.Name)\AppData\Local\Mozilla\Firefox\Profiles\*.default\cache\*.* -Recurse -Force -EA SilentlyContinue -Verbose
+	    Remove-Item -path C:\Users\$($_.Name)\AppData\Local\Mozilla\Firefox\Profiles\*.default\cache2\entries\*.* -Recurse -Force -EA SilentlyContinue -Verbose
+            Remove-Item -path C:\Users\$($_.Name)\AppData\Local\Mozilla\Firefox\Profiles\*.default\thumbnails\* -Recurse -Force -EA SilentlyContinue -Verbose
+            Remove-Item -path C:\Users\$($_.Name)\AppData\Local\Mozilla\Firefox\Profiles\*.default\cookies.sqlite -Recurse -Force -EA SilentlyContinue -Verbose
+            Remove-Item -path C:\Users\$($_.Name)\AppData\Local\Mozilla\Firefox\Profiles\*.default\webappsstore.sqlite -Recurse -Force -EA SilentlyContinue -Verbose
+            Remove-Item -path C:\Users\$($_.Name)\AppData\Local\Mozilla\Firefox\Profiles\*.default\chromeappsstore.sqlite -Recurse -Force -EA SilentlyContinue -Verbose
+            }
+    Write-Host -ForegroundColor yellow "Clearing Mozilla caches"
+    Write-Host -ForegroundColor yellow "Done..."
+    ""
+    "-------------------"
+    # Clear Google Chrome 
+    Write-Host -ForegroundColor Green "SECTION 4: Clearing Google Chrome Caches"
+    "-------------------"
+    Write-Host -ForegroundColor yellow "Clearing Google caches"
+    Write-Host -ForegroundColor cyan
+    Import-CSV -Path C:\users\$env:USERNAME\users.csv -Header Name | foreach {
+            Remove-Item -path "C:\Users\$($_.Name)\AppData\Local\Google\Chrome\User Data\Default\Cache\*" -Recurse -Force -EA SilentlyContinue -Verbose
+			Remove-Item -path "C:\Users\$($_.Name)\AppData\Local\Google\Chrome\User Data\Default\Cache2\entries\*" -Recurse -Force -EA SilentlyContinue -Verbose
+            Remove-Item -path "C:\Users\$($_.Name)\AppData\Local\Google\Chrome\User Data\Default\Cookies" -Recurse -Force -EA SilentlyContinue -Verbose
+            Remove-Item -path "C:\Users\$($_.Name)\AppData\Local\Google\Chrome\User Data\Default\Media Cache" -Recurse -Force -EA SilentlyContinue -Verbose
+            Remove-Item -path "C:\Users\$($_.Name)\AppData\Local\Google\Chrome\User Data\Default\Cookies-Journal" -Recurse -Force -EA SilentlyContinue -Verbose
+			Remove-Item -path "C:\Users\$($_.Name)\AppData\Local\Google\Chrome\User Data\Default\Service Worker\CacheStorage\*" -Recurse -Force -EA SilentlyContinue -Verbose
+			Remove-Item -Path "C:\Users\$($_.Name)\AppData\Local\Google\Chrome\User Data\Snapshots\*" -Recurse -Force -EA SilentlyContinue -Verbose
+			Remove-Item -path "C:\Users\$($_.Name)\AppData\Local\Google\Chrome\User Data\Default\Default\Code Cache\js\*" -Recurse -Force -EA SilentlyContinue -Verbose
+			Remove-Item -path "C:\Users\$($_.Name)\AppData\Local\Google\Chrome\User Data\Default\Default\Cache\*" -Recurse -Force -EA SilentlyContinue -Verbose
+			Remove-Item -path "C:\Users\$($_.Name)\AppData\Local\Google\Chrome\User Data\Default\Default\Service Worker\Cache Storage\*" -Recurse -Force -EA SilentlyContinue -Verbose
+            # Comment out the following line to remove the Chrome Write Font Cache too.
+            # Remove-Item -path "C:\Users\$($_.Name)\AppData\Local\Google\Chrome\User Data\Default\ChromeDWriteFontCache" -Recurse -Force -EA SilentlyContinue -Verbose
+            }
+
+    Write-Host -ForegroundColor yellow "Done..."
+    ""
+    "-------------------"
+    # Clear Internet Explorer
+    Write-Host -ForegroundColor Green "SECTION 5: Clearing Internet Explorer Caches"
+     "-------------------"
+    Write-Host -ForegroundColor yellow "Clearing Google caches"
+    Write-Host -ForegroundColor cyan
+    Import-CSV -Path C:\users\$env:USERNAME\users.csv | foreach {
+            Remove-Item -path "C:\Users\$($_.Name)\AppData\Local\Microsoft\Windows\Temporary Internet Files\*" -Recurse -Force -EA SilentlyContinue -Verbose
+	    Remove-Item -path "C:\Users\$($_.Name)\AppData\Local\Microsoft\Windows\WER\*" -Recurse -Force -EA SilentlyContinue -Verbose
+	    Remove-Item -path "C:\Users\$($_.Name)\AppData\Local\Temp\*" -Recurse -Force -EA SilentlyContinue -Verbose
+	    Remove-Item -path "C:\Windows\Temp\*" -Recurse -Force -EA SilentlyContinue -Verbose
+	    Remove-Item -path "C:\`$recycle.bin\" -Recurse -Force -EA SilentlyContinue -Verbose
+            }
+
+"-------------------"
+    # Clear Edge Chromium 
+    Write-Host -ForegroundColor Green "SECTION 6: Clearing Edge Chromium Caches"
+    "-------------------"
+    Write-Host -ForegroundColor yellow "Clearing Edge Chromium caches"
+    Write-Host -ForegroundColor cyan
+    Import-CSV -Path C:\users\$env:USERNAME\users.csv -Header Name | foreach {
+            Remove-Item -path "C:\Users\$($_.Name)\AppData\Local\Microsoft\Edge\User Data\Default\Service Worker\CacheStorage\*" -Recurse -Force -EA SilentlyContinue -Verbose
+			Remove-Item -path "C:\Users\$($_.Name)\AppData\Local\Microsoft\Edge\User Data\Default\Cache\*" -Recurse -Force -EA SilentlyContinue -Verbose
+			
+			}
+
+    Write-Host -ForegroundColor yellow "Done..."
+    ""
+
+    Write-Host -ForegroundColor yellow "Done..."
+    ""
+	# Remove users CSV now that task is completed
+	Remove-Item -Path C:\users\$env:USERNAME\users.csv -Force -EA SilentlyContinue -Verbose
+    
+	Write-Host -ForegroundColor Green "All Tasks Done!"
+    } else {
+	Write-Host -ForegroundColor Yellow "Session Cancelled"	
+	Exit
+	}
